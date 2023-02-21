157bbb40501c331b167fa6b19f09150dcb317a75
Szabo Bence (XC-DX/ESP2-Bp) <Bence.Szabo@hu.bosch.com>
2023-02-21 17:24:04 +0100

    Revert "Merge pull request #2 from brandonvu12/pre-approval"
    This reverts commit 7d5c8fa6f141c3301b43669f287c912064f464dd, reversing
    changes made to b20d1e8b96d06387d625321de476a5583205222b.

--- src/bloaty.h
+++ src/bloaty.h
@@ -361,6 +361,8 @@ struct RollupRow {
   // The current size of this row (this is not the same as `size` in the case
   // of a diff -- `size` is the delta).
   DomainSizes new_size = {0, 0};
+
+  DomainSizes capacity = {0, 0};
   
   std::vector<RollupRow> sorted_children;
 
